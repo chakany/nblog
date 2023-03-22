f5feb2d6d9825799b36c0281de5e3d1f33c1cb7e
Jack Chakany <jack@chaker.net>
2023-03-22 12:19:44 -0400

    add tag dark mode

--- src/lib/Tag.svelte
+++ src/lib/Tag.svelte
@@ -2,6 +2,6 @@
     export let name: string;
 </script>
 
-<a href="/tags/{name}" target="_self" class="px-2 bg-gray-200 rounded hover:bg-gray-200">
+<a href="/tags/{name}" target="_self" class="px-2 bg-gray-200 hover:bg-gray-300 dark:bg-gray-800 dark:hover:bg-gray-700 rounded">
     {name}
 </a>
