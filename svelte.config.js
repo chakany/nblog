be74bbdb27174ddcc68860d9ad7e44796ba89db0
Jack Chakany <jack@chaker.net>
2023-04-14 09:12:03 -0400

    include version as variable

--- svelte.config.js
+++ svelte.config.js
@@ -1,6 +1,8 @@
 import adapter from "@sveltejs/adapter-auto";
 import preprocess from "svelte-preprocess";
 import { vitePreprocess } from "@sveltejs/kit/vite";
+// to include version
+import pkg from "./package.json" assert { type: "json" };
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
@@ -17,6 +19,9 @@ const config = {
 		// If your environment is not supported or you settled on a specific environment, switch out the adapter.
 		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
 		adapter: adapter(),
+		version: {
+			name: pkg.version,
+		},
 	},
 };
 
