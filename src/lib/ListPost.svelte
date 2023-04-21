e578b4100c375b0aad33b8e6faf57f81f7b22477
Jack Chakany <jack@chaker.net>
2023-04-21 09:40:16 -0400

    Restyle ListPost

--- src/lib/ListPost.svelte
+++ src/lib/ListPost.svelte
@@ -12,40 +12,28 @@
 	const image = getTagValues(post.tags, "image");
 </script>
 
-<div class="font-display">
-	<div class="md:text-md text-xs">
-		Published {formatDistance(
-			new Date(published_at ? Number(published_at[0]) * 1000 : 0),
-			new Date(),
-			{ addSuffix: true }
-	)}
-		{#if post.created_at !== (published_at ? Number(published_at[0]) : 0)}
-			| Edited {formatDistance(new Date(post.created_at * 1000), new Date(), {
-			addSuffix: true,
-		})}
-		{/if}
-		| {readingTime(post.content)} min read
-	</div>
-	<div class="flex">
-		<div class="flex flex-col">
-			<a
-					class="flex cursor-pointer flex-col"
-					href="/posts/{getTagValues(post.tags, 'd')[0]}"
-					target="_self"
-			>
-				<h2 class="text-xl font-bold xl:text-3xl">{title ? title[0] : "Title"}</h2>
-				<p class="subtext">{summary ? summary[0] : "Summary"}</p>
-			</a>
-			<div class="mt-2 flex flex-wrap gap-2">
-				<Tags tags={post.tags.filter((v) => v[0] === "t")} />
+<div class="flex font-display">
+	<div class="flex flex-col my-auto">
+		<a
+				class="flex cursor-pointer flex-col"
+				href="/posts/{getTagValues(post.tags, 'd')[0]}"
+				target="_self"
+		>
+			<h2 class="text-xl font-bold xl:text-3xl">{title ? title[0] : "Title"}</h2>
+			<div class="md:text-md text-xs">
+				{new Date(published_at ? Number(published_at[0]) * 1000 : 0).toLocaleDateString()} <span class="subtext">/</span> {readingTime(post.content)} min read
 			</div>
+			<p class="subtext">{summary ? summary[0] : "Summary"}</p>
+		</a>
+		<div class="mt-2 gap-2">
+			<Tags tags={post.tags.filter((v) => v[0] === "t")} />
 		</div>
-		{#if image}
-			<img
-					class="ml-auto hidden h-20 w-20 rounded object-cover sm:inline"
-					src={image[0]}
-					alt="Banner"
-			/>
-		{/if}
 	</div>
+	{#if image}
+		<img
+				class="ml-auto my-auto hidden h-20 w-20 rounded object-cover sm:inline"
+				src={image[0]}
+				alt="Banner"
+		/>
+	{/if}
 </div>
